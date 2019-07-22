aeb0f0360dffdff2d337e045edf1b645c0d38a54
Rancar <paddyo@gmail.com>
2019-07-22 12:34:36 -0700

    First Version
    First version of the form for doing the color overlay swap

---
+++ VideoOverlayForm/VideoOverlayForm/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VideoOverlayForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Bitmap screen = CaptureScreen();
+            //pictureBox1.Image = screen;
+            //pictureBox1.Load();
+            
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            using (Bitmap bmp = CaptureScreen())
+            {
+
+                // Set the image attribute's color mappings
+                ColorMap[] colorMap = new ColorMap[1];
+                colorMap[0] = new ColorMap();
+                colorMap[0].OldColor = Color.Black;
+                colorMap[0].NewColor = Color.Blue;
+                ImageAttributes attr = new ImageAttributes();
+                attr.SetRemapTable(colorMap);
+                // Draw using the color map
+                Rectangle rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                g.DrawImage(bmp, rect, 0, 0, rect.Width, rect.Height, GraphicsUnit.Pixel, attr);
+            }
+        }
+
+        public static Bitmap CaptureScreen()
+        {
+            Bitmap b = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+            Graphics g = Graphics.FromImage(b);
+            g.CopyFromScreen(0, 0, 0, 0, b.Size);
+            g.Dispose();
+            return b;
+        }
+    }
+}
